a462cffdb749b84cd27793cb1f219f12bb6db4c3
DammySmartSolutions <Dammy4edu@gmail.com>
2026-06-26 16:47:29 -0230

    Add clean Global.asax without license key

---
+++ AutomatedTaskSchedulingSystem/Global.asax.cs
@@ -0,0 +1,88 @@
+﻿using Stimulsoft.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using static System.Collections.Specialized.BitVector32;
+
+namespace AutomatedTaskSchedulingSystem
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            
+           
+
+        }
+
+
+
+
+        public void Register(HttpConfiguration config)
+        {
+
+
+
+
+
+            config.Routes.MapHttpRoute(
+               name: "default",
+               routeTemplate: "api/{controller}/{action}/{id}",
+               defaults: new { action = RouteParameter.Optional, id = RouteParameter.Optional }
+
+               );
+
+
+
+
+        }
+
+        private bool IsWebApiRequest()
+        {
+            return HttpContext.Current.Request.AppRelativeCurrentExecutionFilePath.StartsWith("~/api");
+
+
+        }
+
+
+        protected void Application_PostAuthorizeRequest()
+        {
+
+            if (IsWebApiRequest())
+            {
+                HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
+
+            }
+        }
+
+
+
+
+
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            Session.Timeout = 36000;
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
